d17d8728ee41a7135e6806f4cbd49e5b0d34521e
GitGudSoftEngDev <onlyedupurposes18@gmail.com>
2026-07-24 15:36:44 +0300

    fix(certificates): show generation date on web certificate
    Use modified_date or date_override instead of display_date_for_certificate,
    keeping Robbo Koa certificate_created_date from created_date.

--- lms/djangoapps/certificates/views/webview.py
+++ lms/djangoapps/certificates/views/webview.py
@@ -13,6 +13,7 @@ from uuid import uuid4
 import pytz
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.utils import translation
@@ -30,7 +31,6 @@ from common.djangoapps.util.date_utils import strftime_localized
 from common.djangoapps.util.views import handle_500
 from lms.djangoapps.certificates.api import (
     certificates_viewable_for_course,
-    display_date_for_certificate,
     get_active_web_certificate,
     get_certificate_footer_context,
     get_certificate_header_context,
@@ -114,22 +114,16 @@ def _update_certificate_context(context, course, course_overview, user_certifica
         suffix=context.get('certificate_verify_url_suffix')
     )
 
-    # We prefer a CourseOverview for this function because it validates and corrects certificate_available_date
-    # and certificates_display_behavior values. However, not all certificates are guaranteed to have a CourseOverview
-    # associated with them, so we fall back on the course in that case. This shouldn't cause a problem because courses
-    # that are missing CourseOverviews are generally old courses, and thus their display values are no longer relevant
-    if course_overview:
-        date = display_date_for_certificate(course_overview, user_certificate)
-    else:
-        date = display_date_for_certificate(course, user_certificate)
+    # Issue date on the web certificate: when it was generated (not course end / available date).
+    try:
+        date = user_certificate.date_override.date
+    except ObjectDoesNotExist:
+        date = user_certificate.modified_date
     # Translators:  The format of the date includes the full name of the month
     context['certificate_date_issued'] = strftime_localized(date, settings.CERTIFICATE_DATE_FORMAT)
     # Robbo cert sheet: creation date, day-first (e.g. "3 июля 2026")
-    created = getattr(user_certificate, 'created_date', None) or getattr(user_certificate, 'created', None)
-    if created:
-        context['certificate_created_date'] = strftime_localized(created, '%-d %B %Y')
-    else:
-        context['certificate_created_date'] = strftime_localized(date, '%-d %B %Y')
+    created = getattr(user_certificate, 'created_date', None) or getattr(user_certificate, 'created', None) or date
+    context['certificate_created_date'] = strftime_localized(created, '%-d %B %Y')
 
     # Translators:  This text represents the verification of the certificate
     context['document_meta_description'] = _('This is a valid {platform_name} certificate for {user_name}, '
